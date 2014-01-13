4e65fc88059bd233e3477ef96f775da5e0432794
Keshi Dai <daikeshi@gmail.com>
2014-01-13 10:37:57 -0500

    Some refactoring

--- models/bloomfilter.py
+++ models/bloomfilter.py
@@ -1,12 +1,14 @@
-from bitarray import bitarray
 import mmh3
 import math
+
+from bitarray import bitarray
+
 
 LN2 = math.log(2)
 
 
 class BloomFilter:
-    def __init__(self, size, hash_count):
+    def __init__(self, size=10, hash_count=2):
         self.size = size
         self.hash_count = hash_count
         self.bit_array = bitarray(size)
@@ -14,26 +16,31 @@ class BloomFilter:
 
     def add(self, key):
         for seed in xrange(self.hash_count):
-            result = mmh3.hash(key, seed) % self.size
+            result = hash(key, seed) % self.size
             self.bit_array[result] = 1
 
-    def get(self, key):
+    def __contains__(self, key):
         for seed in xrange(self.hash_count):
-            result = mmh3.hash(key, seed) % self.size
+            result = hash(key, seed) % self.size
             if self.bit_array[result] == 0:
                 return False
         return True
 
-
-def estimate_size(n, p):
-    return -n * math.log(p) / (LN2 * LN2)
-
-
-def estimate_hash_count(n, m):
-    return m * LN2 / float(n)
+    @staticmethod
+    def make_hash(key, seed):
+        # map 32-bit hash value to an unsigned integer
+        return mmh3.hash(key, seed) & 0xffffffff
 
 
-def estimate_parameters(n, p):
-    m = estimate_size(n, p)
-    k = estimate_hash_count(n, m)
+def estimate_size(capacity, error_rate):
+    return -capacity * math.log(error_rate) / (LN2 * LN2)
+
+
+def estimate_hash_count(capacity, hash_size):
+    return hash_size * LN2 / float(capacity)
+
+
+def estimate_parameters(capacity, error_rate):
+    m = estimate_size(capacity, error_rate)
+    k = estimate_hash_count(capacity, m)
     return m, k
